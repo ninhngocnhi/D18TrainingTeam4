4c87665d278c2e692d26a84b42925bc57c97a4b2
ninhngocnhi <43540391+ninhngocnhi@users.noreply.github.com>
2018-09-26 17:28:06 +0700

    tuần 1
    #include <stdio.h>
    
    int main() {
     int a;
     long long b;
     char c;
     float d;
     double e;
     scanf ("%d %lld %c %3f %9lf", &a, &b, &c, &d, &e);
     printf ("%d %lld %c %3f %9lf", a, b, c, d, e);
    	return 0;
    }

---
+++ AnhNgoc/Untitled1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int a;
+ long long b;
+ char c;
+ float d;
+ double e;
+ scanf ("%d %lld %c %3f %9lf", &a, &b, &c, &d, &e);
+ printf ("%d %lld %c %3f %9lf", a, b, c, d, e);
+	return 0;
+}
+
