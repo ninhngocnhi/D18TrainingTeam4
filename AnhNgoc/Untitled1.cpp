1ec6392c15501f61ad00ddca8a056f6eb5654772
ninhngocnhi <43540391+ninhngocnhi@users.noreply.github.com>
2018-09-26 17:32:04 +0700

    bài 1

--- AnhNgoc/Untitled1.cpp
+++ AnhNgoc/Untitled1.cpp
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
 int main() {
- int a;
- long long b;
- char c;
- float d;
- double e;
- scanf ("%d %lld %c %3f %9lf", &a, &b, &c, &d, &e);
- printf ("%d %lld %c %3f %9lf", a, b, c, d, e);
+ printf (" ngon ngu lap trinh C");
 	return 0;
 }
 
