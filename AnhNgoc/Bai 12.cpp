d4925af40701d36a7959946dbf3f4dbd416af1d6
ninhngocnhi <43540391+ninhngocnhi@users.noreply.github.com>
2018-10-31 16:07:51 +0700

    bài 12

---
+++ AnhNgoc/Bai 12.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	long long a;
+		printf ("nhap so : ");3
+	scanf("%lld",&a);
+	if (a%2 == 0) {
+		printf ("even");
+		}
+	else {
+		printf("odd");
+	}
+	return 0;
+}
+
