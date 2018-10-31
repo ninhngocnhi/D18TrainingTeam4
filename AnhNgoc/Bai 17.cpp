8f0f17b3555d2ce226973a8528d91f36c00ba5c4
ninhngocnhi <43540391+ninhngocnhi@users.noreply.github.com>
2018-10-31 16:10:44 +0700

    bài 17

---
+++ AnhNgoc/Bai 17.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+	long long a;
+	printf ("nhap tao xem nao: ");
+	scanf ("%lld", &a);
+	if (a==0){
+		printf ("zero");
+	}
+	if (a==1){
+		printf ("one");
+	}
+	if (a==2){
+		printf ("two");
+	}  
+	if (a==3){
+		printf ("three");
+	}
+	if (a==4){
+		printf ("four");
+	}
+	if (a==5){
+		printf ("five");
+	}
+	if (a==6){
+		printf ("six");
+	}
+	if (a==7){
+		printf ("seven");
+	}
+	if (a==8){
+		printf ("eight");
+	}
+	if (a==9){
+		printf ("nine");
+	}
+	if (a==10){
+		printf ("ten");
+	}
+	if (a>10){
+		printf ("greater than ten");
+	}
+	return 0;
+}
+
